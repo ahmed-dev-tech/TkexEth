648827f53a671d8071bdedbde433677b99482109
ahmed-dev-tech <76089213+ahmed-dev-tech@users.noreply.github.com>
2022-06-13 15:46:07 +0500

    rem

--- TkexEth-1f2def14d4fd6439f4849a25cfc74a4d2abc1655/src/components/index.js
+++
@@ -1,3 +0,0 @@
-import Card from "./card/Card.js";
-
-export {Card}
--- TkexEth-1f2def14d4fd6439f4849a25cfc74a4d2abc1655/src/dummyData/index.js
+++
@@ -1,5 +0,0 @@
-import { cardData } from "./cardData";
-import {howData } from "./howData";
-import { ecoCard } from "./ecoCardData";
-
-export { cardData, howData, ecoCard }
